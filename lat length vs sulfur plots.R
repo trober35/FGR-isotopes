ccb90b39b4ed4fb565d706bd86fa507c6d2f434b
trober35 <trober35@uwyo.edu>
2020-09-11 12:39:27 -0600

    c

---
+++ lat length vs sulfur plots.R
@@ -0,0 +1,34 @@
+#lat length vs S
+plot(NULL, xlim=c(180,800), ylim= c(0,8), xlab="Length in mm", ylab=expression(paste(delta^{34}, "S")),main="Length vs.sulfur",cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
+
+
+points(lat$length, lat$S, pch=16, col="blue")
+
+points(lat.canyon$length, lat.canyon$S, cex=(lat.canyon$age)/8)
+points(lat.inflow$length, lat.inflow$S, cex=(lat.inflow$age)/8, pch=16, col="darkblue")
+points(lat.oh$length, lat.oh$S, cex=(lat.oh$age)/8, pch=16, col="goldenrod")
+
+lat.canyon<-subset(lat, lat$region=="canyon")
+lat.inflow<-subset(lat, lat$region=="inflow")
+lat.oh<-subset(lat, lat$region=="open_hills")
+
+#sulfur vs length separated by region
+plot(NULL, xlim=c(180,800), ylim= c(0,8), xlab="Length in mm", ylab=expression(paste(delta^{34}, "S")),main="LAT Length vs.Sulfur",cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
+points(lat$length, lat$S, pch=16, col="blue")
+#canyon
+plot(NULL, xlim=c(180,800), ylim= c(0, 8), xlab="Length in mm", ylab=expression(paste(delta^{13}, "C")),main="LAT Length vs.Sulfur Canyon",cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
+points(lat.canyon$length, lat.canyon$S, cex=(lat.canyon$age)/8,pch= 16, col="red")
+#inflow
+plot(NULL, xlim=c(180,800), ylim= c(0,8), xlab="Length in mm", ylab=expression(paste(delta^{13}, "C")),main="LAT Length vs.Sulfur Inflow",cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
+points(lat.inflow$length, lat.inflow$S, cex=(lat.inflow$age)/8, pch=16, col="darkblue")
+#open hills
+plot(NULL, xlim=c(180,800), ylim= c(0,8), xlab="Length in mm", ylab=expression(paste(delta^{13}, "C")),main="LAT Length vs.Sulfur Open Hills",cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
+points(lat.oh$length, lat.oh$S, cex=(lat.oh$age)/8, pch=16, col="goldenrod")
+
+#all together
+plot(NULL, xlim=c(180,800), ylim= c(0,8), xlab="Length in mm", ylab=expression(paste(delta^{34}, "S")),main="LAT Length vs.Sulfur",cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
+points(lat.canyon$length, lat.canyon$S, cex=(lat.canyon$age)/8,pch= 16, col="red")
+points(lat.inflow$length, lat.inflow$S, cex=(lat.inflow$age)/8, pch=16, col="darkblue")
+points(lat.oh$length, lat.oh$S, cex=(lat.oh$age)/8, pch=16, col="goldenrod")
+
+legend(200,8, legend=c("Inflow","Open Hills", "Canyon"), pch=c(16,16,16), col=c("red","dark blue", "goldenrod"))
